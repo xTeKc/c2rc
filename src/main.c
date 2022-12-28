f7201a92577c07204aede565c5e284b85b2c4e3d
xTeKc <bcd3v@pm.me>
2022-12-28 17:19:52 -0500

    .

--- src/main.c
+++ src/main.c
@@ -1,28 +1,28 @@
 #include <stdio.h>
 #include <stdint.h>
 
-int add(int c_input_one, int c_input_two) {
+uint32_t add(uint32_t c_input_one, uint32_t c_input_two) {
     printf("[C] input one is: %i \n", c_input_one);
     printf("[C] input two is: %i \n", c_input_two);
 
     return c_input_one + c_input_two;
 }
 
-int sub(int c_input_one, int c_input_two) {
+uint32_t sub(uint32_t c_input_one, uint32_t c_input_two) {
     printf("[C] input one is: %i \n", c_input_one);
     printf("[C] input two is: %i \n", c_input_two);
 
     return c_input_one - c_input_two;
 }
 
-int mul(int c_input_one, int c_input_two) {
+uint32_t mul(uint32_t c_input_one, uint32_t c_input_two) {
     printf("[C] input one is: %i \n", c_input_one);
     printf("[C] input two is: %i \n", c_input_two);
 
     return c_input_one * c_input_two;
 }
 
-int div(int c_input_one, int c_input_two) {
+uint32_t div(uint32_t c_input_one, uint32_t c_input_two) {
     printf("[C] input one is: %i \n", c_input_one);
     printf("[C] input two is: %i \n", c_input_two);
 
